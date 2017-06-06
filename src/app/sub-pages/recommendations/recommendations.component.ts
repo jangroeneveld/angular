d868894fdb6ed898eb9171e12be35534e2c7cb58
DESKTOP-GE8A2BK\Misha <michakiber1@gmail.com>
2017-06-06 13:45:05 +0200

    Added jqery, Added fonts to everyting

--- src/app/sub-pages/recommendations/recommendations.component.ts
+++ src/app/sub-pages/recommendations/recommendations.component.ts
@@ -1,12 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
     selector: 'recommendations',
     templateUrl: './recommendations.component.html',
     styleUrls: ['recommendations.component.scss']
 })
-export class RecommendationsComponent{
+export class RecommendationsComponent implements OnInit{
     consturctor () {
         
+    }
+    ngOnInit(){
+        var blocks = document.getElementsByClassName('block');
+
+        for(var i = 0; i < blocks.length; i++){
+            blocks[i].addEventListener('click', rescale);
+        }
+
+        function rescale(event){
+            for(var i = 0; i < event.target.parentElement.children.length; i++){
+                event.target.parentElement.children[i].style = "flex-grow: 1";
+            }
+            event.target.style.flexGrow = 3;
+        }
     }
 }
