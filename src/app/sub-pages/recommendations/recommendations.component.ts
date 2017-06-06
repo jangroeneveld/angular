c6279bc34c0c9375bccdd4a76dad0b3697492818
DESKTOP-GE8A2BK\Misha <michakiber1@gmail.com>
2017-06-06 14:52:59 +0200

    Added jfiddle thing to blocks

--- src/app/sub-pages/recommendations/recommendations.component.ts
+++ src/app/sub-pages/recommendations/recommendations.component.ts
@@ -6,19 +6,19 @@ import * as $ from 'jquery';
     templateUrl: './recommendations.component.html',
     styleUrls: ['recommendations.component.scss']
 })
-export class RecommendationsComponent implements OnInit{
-    consturctor () {
-        
+export class RecommendationsComponent implements OnInit {
+    consturctor() {
+
     }
-    ngOnInit(){
+    ngOnInit() {
         var blocks = document.getElementsByClassName('block');
 
-        for(var i = 0; i < blocks.length; i++){
+        for (var i = 0; i < blocks.length; i++) {
             blocks[i].addEventListener('click', rescale);
         }
 
-        function rescale(event){
-            for(var i = 0; i < event.target.parentElement.children.length; i++){
+        function rescale(event) {
+            for (var i = 0; i < event.target.parentElement.children.length; i++) {
                 event.target.parentElement.children[i].style = "flex-grow: 1";
             }
             event.target.style.flexGrow = 3;
