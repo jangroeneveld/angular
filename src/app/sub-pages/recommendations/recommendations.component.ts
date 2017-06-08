24dbefdac29ff25a71f917010f1745bf07b4966a
DESKTOP-GE8A2BK\Misha <michakiber1@gmail.com>
2017-06-08 13:34:25 +0200

    Bugfixes whith the flexGrowth

--- src/app/sub-pages/recommendations/recommendations.component.ts
+++ src/app/sub-pages/recommendations/recommendations.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
-import * as $ from 'typed.js';
 import { RestaurantsService } from '../../services/restaurants.service';
+import * as $ from 'typed.js';
 
 @Component({
     selector: 'recommendations',
@@ -32,12 +32,12 @@ export class RecommendationsComponent {
 
     rescale = (event) => {
         for(let i = this.lastBlocks.length -1; i >= 0; i--){
-            this.lastBlocks[i].style = "";
+            this.lastBlocks[i].style.flexGrow = "";
             this.lastBlocks.pop();
         }
         
         for (let i = 0; i < event.target.parentElement.children.length; i++) {
-            event.target.parentElement.children[i].style = "flex-grow: 1";
+            event.target.parentElement.children[i].style.flexGrow = 1;
             this.lastBlocks.push(event.target.parentElement.children[i]);
         }
         event.target.style.flexGrow = 3;
